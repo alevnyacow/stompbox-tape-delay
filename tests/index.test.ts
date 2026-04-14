0011f8fb6a88df248f02b03b14d2404cb609d932
alevny <alevnyacow@gmail.com>
2026-04-14 12:18:20 +0400

    test from readme

--- tests/index.test.ts
+++ tests/index.test.ts
@@ -1,15 +1,28 @@
 import { expect, test } from '@rstest/core';
-import { TapeDelay } from '../src/index';
+import { TapeDelay, injectable } from '../src/index';
 
-class A {
-  sup = () => 'sup'
-}
+@injectable()
+class A { hello = () => 'hello' }
 
-const a = new TapeDelay({
-  A
+@injectable()
+class TestB { method = () => 'test' }
+
+@injectable()
+class DevB { method = () => 'dev' }
+
+@injectable()
+class ProdB { method = () => 'prod' }
+
+const container = new TapeDelay({
+    // one implementation for all environments
+    A,
+    // test, dev and prod environment implementations
+    B: [TestB, DevB, ProdB]
 })
 
 test('happy path', () => {
-  const aInstance = a.instance('A')
-  expect(aInstance.sup()).toBe('sup')
+  const aInstance = container.instance('A')
+  expect(aInstance.hello()).toBe('hello')
+  const bInstance = container.instance('B')
+  expect(bInstance.method()).toBe('test')
 });
